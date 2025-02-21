e0e155f6c097e604a8e35553d586e1810592eac1
Ned Bingham <edward.bingham@broccolimicro.io>
2025-02-20 21:43:56 -0500

    making output sensitive

--- deploy/00_bootstrap/output.tf
+++ deploy/00_bootstrap/output.tf
@@ -1,13 +1,11 @@
-
-# Set `sensitive = true` if this will be used in automation!
 output "access_key_id" {
   value     = aws_iam_access_key.terraform_key.id
-  # sensitive = true
+  sensitive = true
 }
 
 output "secret_access_key" {
   value     = aws_iam_access_key.terraform_key.secret
-  # sensitive = true
+  sensitive = true
 }
 
 output "s3_bucket_name" {
